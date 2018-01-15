ae44da74cba4f23bd1a633d59e57cf74adcbdd15
Anton Bäckström <antonbcm@gmail.com>
2018-01-15 11:16:45 +0100

    Started on the snake

--- snake/logic.js
+++ snake/logic.js
@@ -1,7 +1,12 @@
-const dimensions = 10;
-const snake = [];
-const apple = [];
+let dimensions = 10;
+let snake = [];
+let food = [];
 let timer;
+
+const FOOD = 0;
+const SNAKE = 1;
+const HEAD = 2;
+const EMPTY = 3;
 
 function $(input) {
   if (input.charAt(0) === '.') {
@@ -13,6 +18,20 @@ function $(input) {
   }
 
   return document.getElementsByTagName(input);
+}
+
+function setTile(y, x, type) {
+  if (type === FOOD) {
+    food = [y, x];
+    $(`#${y},${x}`).className = 'food';
+  } else if (type === SNAKE) {
+    $(`#${y},${x}`).className = 'snake';
+  } else if (type === HEAD) {
+    snake.push([y, x]);
+    $(`#${y},${x}`).className = 'head';
+  } else if (type === EMPTY) {
+    $(`#${y},${x}`).className = 'empty';
+  }
 }
 
 function setup() {
@@ -30,6 +49,13 @@ function setup() {
   $('#board').innerHTML = html;
 
   // create Snake
-  // create apple
+  snake = [[0, 0], [0, 1], [0, 2]];
+  for (const tile of snake) {
+    setTile(tile[0], tile[1], SNAKE);
+  }
+
+  setTile(snake[snake.length - 1][0], snake[snake.length - 1][1], HEAD);
+
+  // create food
   // create timer
 }
