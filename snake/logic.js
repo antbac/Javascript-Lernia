377e58bf73e2633213e8c83151f0a69223a051a1
Anton Bäckström <antonbcm@gmail.com>
2018-01-15 11:48:13 +0100

    Currently buggy version

--- snake/logic.js
+++ snake/logic.js
@@ -1,4 +1,5 @@
 let dimensions = 10;
+let direction;
 let snake = [];
 let food = [];
 let timer;
@@ -7,6 +8,11 @@ const FOOD = 0;
 const SNAKE = 1;
 const HEAD = 2;
 const EMPTY = 3;
+
+const UP = 10;
+const DOWN = 11;
+const RIGHT = 12;
+const LEFT = 13;
 
 function $(input) {
   if (input.charAt(0) === '.') {
@@ -34,6 +40,31 @@ function setTile(y, x, type) {
   }
 }
 
+function move() {
+  const head = snake[snake.length - 1];
+  console.log(head);
+  let [y, x] = head;
+  switch (direction) {
+    case RIGHT:
+      x += 1;
+      break;
+    case LEFT:
+      x -= 1;
+      break;
+    case UP:
+      y -= 1;
+      break;
+    case DOWN:
+      y += 1;
+      break;
+  }
+  setTile(head[0], head[1], SNAKE);
+  snake.push([y, x]);
+  setTile(y, x, HEAD);
+  setTile(snake[0][0], snake[0][1], EMPTY);
+  snake.shift(); // Remove the first element
+}
+
 function setup() {
   // board
   let html = '';
@@ -49,6 +80,7 @@ function setup() {
   $('#board').innerHTML = html;
 
   // create Snake
+  direction = RIGHT;
   snake = [[0, 0], [0, 1], [0, 2]];
   for (const tile of snake) {
     setTile(tile[0], tile[1], SNAKE);
@@ -58,4 +90,21 @@ function setup() {
 
   // create food
   // create timer
+  timer = setInterval(move, 350);
+
 }
+
+window.onkeyup = function (e) {
+  const key = e.keyCode ? e.keyCode : e.which;
+
+  //console.log('You pressed: ' + key);
+  if (key === 37) { // LEFT
+    direction = LEFT;
+  } else if (key === 38) { // UP
+    direction = UP;
+  } else if (key === 39) { // RIGHT
+    direction = RIGHT;
+  } else if (key === 40) { // DOWN
+    direction = DOWN;
+  }
+};
